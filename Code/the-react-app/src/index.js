77ab3e9e6866147731f57bb32ff91c4ca83a6f83
Anil Joseph <anil.jos@gmail.com>
2020-05-15 08:57:19 +0530

    revert error

--- Code/the-react-app/src/index.js
+++ Code/the-react-app/src/index.js
@@ -4,19 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(
-//    <React.StrictMode>
-//      <App />,
-//     </React.StrictMode>,
-//   document.getElementById('root')
-// );
+ReactDOM.render(
+   <React.StrictMode>
+     <App />,
+    </React.StrictMode>,
+  document.getElementById('root')
+);
 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  ReactDOM.hydrate(<App />, rootElement);
-} else {
-  ReactDOM.render(<App />, rootElement);
-}
 
 
 
